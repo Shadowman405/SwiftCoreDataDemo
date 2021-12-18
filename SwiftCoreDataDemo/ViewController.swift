40edca8fd87d51b269bbde24c82a9db90e5c70cc
Maxim Mitin <maximus405@gmail.com>
2021-12-18 17:43:07 +0300

    Initial Commit

---
+++ SwiftCoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCoreDataDemo
+//
+//  Created by Maxim Mitin on 18.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
